07ddb21a0cd63069ee835097b9ff6e333fe1a7d5
Naman V Verma <128792351+officialnamanverma@users.noreply.github.com>
2023-04-29 23:52:19 +0530

    Update Naman.java

--- Naman.java
+++ Naman.java
@@ -2,6 +2,7 @@ class Naman
 {  
 public static void main(String args[])  
 {  
-System.out.println("Hello NAMAN");
-}  
-}  
+System.out.println("Hello Sir");
+System.out.println("Hello Madam");
+}
+}
